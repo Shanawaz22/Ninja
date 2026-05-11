9f8672bb8d554769cc9c4a605f8eccc4b0fd5599
shanawaz SH <shanawazshan79@gmial.com>
2026-05-11 20:44:44 +0530

    created registertest

---
+++ src/test/java/tests/RegisterTest.java
@@ -0,0 +1,48 @@
+
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pages.RegisterPage;
+import utils.ConfigReader;
+import utils.DriverSetup;
+import utils.ExtentManager;
+
+public class RegisterTest {
+
+    WebDriver driver;
+    RegisterPage rp;
+
+    @BeforeMethod
+    public void setup() {
+        ConfigReader.load();
+        driver = DriverSetup.getDriver();
+        rp = new RegisterPage(driver);
+    }
+
+    @AfterMethod
+    public void teardown() {
+        DriverSetup.quitDriver();
+    }
+
+    @Test
+    public void validRegister() {
+        ExtentManager.createTest("validRegister");
+        rp.goTo();
+        String em = "user" + System.currentTimeMillis() + "@gmail.com";
+        rp.register("Test", "User", em, "9876543210", "Test@1234");
+        Assert.assertTrue(driver.getCurrentUrl().contains("account"));
+        ExtentManager.getTest().pass("Registration successful");
+    }
+
+    @Test
+    public void emptyRegister() {
+        ExtentManager.createTest("emptyRegister");
+        rp.goTo();
+        rp.register("", "", "", "", "");
+        Assert.assertTrue(rp.getError().contains("Privacy Policy"));
+        ExtentManager.getTest().pass("Error shown for empty fields");
+    }
+
+}
