1eb680c15d8941be52b6c67c38260937ffc39300
shanawaz SH <shanawazshan79@gmial.com>
2026-05-11 16:40:24 +0530

    created login page

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,41 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "input-email")
+    WebElement emailField;
+
+    @FindBy(id = "input-password")
+    WebElement passwordField;
+
+    @FindBy(css = "input[value='Login']")
+    WebElement loginBtn;
+
+    @FindBy(css = ".alert-danger")
+    WebElement errorMsg;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void goTo() {
+        driver.get(utils.ConfigReader.get("url") + "/index.php?route=account/login");
+    }
+
+    public void login(String email, String pass) {
+        type(emailField, email);
+        type(passwordField, pass);
+        click(loginBtn);
+    }
+
+    public String getError() {
+        return getText(errorMsg);
+    }
+
+}
