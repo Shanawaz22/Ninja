664306a43b27e9135ea286610eb38f114592aea8
shanawaz SH <shanawazshan79@gmial.com>
2026-05-11 16:34:12 +0530

    the config reader class has been initialized

---
+++ src/test/java/utils/ConfigReader.java
@@ -0,0 +1,24 @@
+
+package utils;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    static Properties prop = new Properties();
+
+    public static void load() {
+        try {
+            FileInputStream f = new FileInputStream("src/test/resources/config.properties");
+            prop.load(f);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String get(String key) {
+        return prop.getProperty(key);
+    }
+
+}
