1b02509e3c86732df8e17ed3a29935a828039ad8
shanawaz SH <shanawazshan79@gmial.com>
2026-05-11 20:48:59 +0530

    created checkouttest

---
+++ src/test/java/tests/CheckoutTest.java
@@ -0,0 +1,49 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pages.CheckoutPage;
+import pages.LoginPage;
+import utils.ConfigReader;
+import utils.DriverSetup;
+import utils.ExtentManager;
+
+public class CheckoutTest {
+
+    WebDriver driver;
+    CheckoutPage chp;
+    LoginPage lp;
+
+    @BeforeMethod
+    public void setup() {
+        ConfigReader.load();
+        driver = DriverSetup.getDriver();
+        chp = new CheckoutPage(driver);
+        lp = new LoginPage(driver);
+    }
+
+    @AfterMethod
+    public void teardown() {
+        DriverSetup.quitDriver();
+    }
+
+    @Test
+    public void checkoutWithoutLogin() {
+        ExtentManager.createTest("checkoutWithoutLogin");
+        chp.goTo();
+        Assert.assertTrue(driver.getCurrentUrl().contains("login"));
+        ExtentManager.getTest().pass("Redirected to login");
+    }
+
+    @Test
+    public void checkoutWithLogin() {
+        ExtentManager.createTest("checkoutWithLogin");
+        lp.goTo();
+        lp.login(ConfigReader.get("email"), ConfigReader.get("password"));
+        chp.goTo();
+        Assert.assertTrue(driver.getCurrentUrl().contains("checkout"));
+        ExtentManager.getTest().pass("Checkout page loaded");
+    }
+
+}
